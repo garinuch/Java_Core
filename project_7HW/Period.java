91db776bf53db896ba3cd53c936bcc9fefb4ab55
garinuch <83226049+garinuch@users.noreply.github.com>
2022-01-26 09:13:43 +0300

    Delete project_7HW directory

--- project_7HW/Period.java
+++
@@ -1,5 +0,0 @@
-package project_7HW;
-
-public enum Period {
-    FIVE_DAYS
-}
--- project_7HW/WeatherModel.java
+++
@@ -1,8 +0,0 @@
-package project_7HW;
-
-import java.io.IOException;
-
-public interface WeatherModel {
-    default void getWeather(String selectedCiti, Period period) throws IOException {
-    }
-}
