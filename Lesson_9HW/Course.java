383402cb2556f64a00f3aa91500a86cec2b9ad03
garinuch <83226049+garinuch@users.noreply.github.com>
2022-02-07 05:56:18 +0300

    Add files via upload

---
+++ Lesson_9HW/Course.java
@@ -0,0 +1,32 @@
+package Lesson_9HW;
+
+import java.util.Objects;
+
+public class Course {
+    private String name;
+
+    public Course(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Course course = (Course) o;
+        return name.equals(course.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+}
---
+++ Lesson_9HW/Main.java
@@ -0,0 +1,74 @@
+package Lesson_9HW;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Student> studentList = new ArrayList<>();
+
+        studentList.add(new Student("Oleg", Arrays.asList(
+                new Course("MATH"),
+                new Course("Biology"))));
+        studentList.add(new Student("Anton", Arrays.asList(
+                new Course("BIOLOGY"),
+                new Course("GEOGRAPHY"))));
+        studentList.add(new Student("Oksana", Arrays.asList(
+                new Course("GEOGRAPHY"),
+                new Course("MATH"))));
+        studentList.add(new Student("Aleksey", Arrays.asList(
+                new Course("TESTING"),
+                new Course("Biology"))));
+        studentList.add(new Student("Andrey", Arrays.asList(
+                new Course("GEOGRAPHY"),
+                new Course("TESTING"))));
+        studentList.add(new Student("Oleg", Arrays.asList(
+                new Course("MATH"),
+                new Course("TESTING"))));
+        studentList.add(new Student("Vera", Arrays.asList(
+                new Course("GEOGRAPHY"),
+                new Course("Biology"))));
+
+        System.out.println(studentList.stream()
+                .map(s -> s.getCourses())
+                .flatMap(f -> f.stream())
+                .collect(Collectors.toList()));
+
+        Arrays students;
+        System.out.println(studentList.stream()
+                .sorted((s1, s2) -> s2.getCourses().size() - s1.getCourses().size())
+                .limit(2)
+                .collect(Collectors.toList()));
+
+        Course course = new Course("GEOGRAPHY");
+        System.out.println(studentList.stream()
+                .filter(s -> s.getCourses().contains(course))
+                .collect(Collectors.toList()));
+
+    //==========================================================
+        Stream<String> citiesStream = Stream.of("Париж", "Лондон", "Мадрид", "Берлин", "Брюссель");
+        citiesStream.forEach(System.out::println);
+    //========================================================
+        //  Вывод всех студентов.
+        Stream<List<Student>> res = Stream.of(studentList);
+        studentList.forEach(System.out::println);
+
+    //==========================================================
+        List<Student> StudentsList = studentList.stream().distinct().collect(Collectors.toList());
+        System.out.println("Уникальный список данных = "+ StudentsList);
+    //===========================================================
+//  Вывод каждое 2ое значение.
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        numbers.stream()
+                .filter(i -> i % 2 == 0)
+                .forEach(System.out::println);
+// Вывод студентов по именам.
+        studentList.stream()
+                .map(Student::getName)
+                .forEach(System.out::println);
+    }
+}
